75ac7c33e03c8d213e37ab238f3449b9465fc35d
Esther Mutua <155438090+Esther-pixel31@users.noreply.github.com>
2024-09-21 01:04:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-function display(response) {
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#current-temperature");
+    let temperature = Math.round(response.data.temperature.current);
     let cityElement = document.querySelector("#current-city");
     cityElement.innerHTML = response.data.city;
-    let tempElement = document.querySelector(".current-temperature-value");
-    tempElement.innerHTML = Math.round(response.data.temperature.current);
+    temperatureElement.innerHTML = temperature;
   }
   
   function search(event) {
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-input");
     let city = searchInputElement.value;
+  
     let apiKey = "8d35b60a4af3ao266f083t4e99acb1e4";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(display);
+  
+    axios.get(apiUrl).then(displayTemperature);
   }
   
   function formatDate(date) {
@@ -34,7 +37,7 @@ function display(response) {
       "Wednesday",
       "Thursday",
       "Friday",
-      "Saturday",
+      "Saturday"
     ];
   
     let formattedDay = days[day];
